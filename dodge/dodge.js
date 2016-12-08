920f57c1612dc429867c426149097953c7d0b8d4
Jake Ludington <durzaskillz@gmail.com>
2016-12-08 16:45:20 -0500

    save

--- dodge/dodge.js
+++ dodge/dodge.js
@@ -45,7 +45,7 @@ class Load {
   preload() {
     console.log("Loading");
     this.load.image("bg",C.bg.file)
-    this.load.spritesheet("player",C.p.file,width,C.p.frames)
+    this.load.spritesheet("player",C.p.file.width,C.p.frames)
     this.load.spritesheet("dodge",C.d.width,C.d.height,C.d.frames);
   }
   
