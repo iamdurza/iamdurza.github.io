b42d0306e9ee2a1c62cd9931b32ced768bb33afc
Jake Ludington <durzaskillz@gmail.com>
2016-12-15 17:07:32 -0500

    save

--- dodge/dodge.js
+++ dodge/dodge.js
@@ -92,10 +92,7 @@ class Play {
       this.dodge.y = C.d.starty
       let px = (C.d.width * this.dodge.scale.x) / 2;
       let max = C.game.width - px
-      let min = 0 + px
-      let newx = randInt(max);
-      if (newx < min){ newx = min }
-      this.dodge.x = newx
+      this.dodge.x = randInt(px,max);
     }
       this.dodge.y += C.d.speed;
   }
@@ -111,7 +108,7 @@ function restart() {
 
 
 function randInt(max) {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 var game = new Phaser.Game(C.game.width,C.game.height);
